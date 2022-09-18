b765a762770186144af04ec2f755d12c7abf231a
anoushk1234 <anoushka1828@gmail.com>
2022-09-18 18:12:56 +0530

    ts ignore

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 const withTM = require("next-transpile-modules")(["nextjs-components"]);
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = withTM(nextConfig);
