08eca0282ce459a2ff976d6f9bb153d4aac5f310
anoushk1234 <anoushka1828@gmail.com>
2022-10-13 13:29:10 +0530

    document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,7 @@ import Document, {
 import { CssBaseline } from "@nextui-org/react";
 import { ReactElement } from "react";
 import Cursor from "../components/cursor";
+import Script from "next/script";
 
 class MyDocument extends Document {
   static async getInitialProps(
@@ -27,8 +28,8 @@ class MyDocument extends Document {
         <Head>{CssBaseline.flush()}</Head>
         <body>
           <Main />
-
           <NextScript />
+          <Script src="/theme.js" strategy="beforeInteractive" />
         </body>
       </Html>
     );
